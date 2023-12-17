89b899bf7afc7d3748f58df26ecf085a050644dd
md98uddin <mduddin1998@yahoo.com>
2023-12-17 02:57:51 -0500

    added images for services

--- src/components/screens/PageNotFound.js
+++ src/components/screens/PageNotFound.js
@@ -3,7 +3,7 @@ import React from "react";
 export const PageNotFound = () => {
   return (
     <div>
-      <h1 className="header">404. Page Not Found</h1>
+      <h1 className="request-header title-text">404. Page Not Found</h1>
     </div>
   );
 };
--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer style={{ position: "static", margin: 0 }}>
       <p>&copy; 2023 Your Company. All rights reserved.</p>
     </footer>
   );
